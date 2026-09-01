844ea2adcb1b0362b996c9a8583b14b1fe811414
Humi145 <gaels9555@gmail.com>
2026-09-01 16:11:59 +0200

    feat: removing the 'Géoemploi' sign part

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,12 +1,9 @@
 import Map from "../components/Map"
 
-export default function PageMap() {
+export default function Home() {
     return (
         <div className="flex flex-col flex-1 bg-zinc-50 font-sans bg-main-2">
             <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white bg-main-2 sm:items-start">
-                <h1 className="text-black text-[50px]">
-                    Bienvenue sur <span className="font-semibold">GéoEmploi</span>!
-                </h1>
                 <Map />
             </main>
         </div>
