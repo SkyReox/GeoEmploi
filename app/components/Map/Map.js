bef2f6c6ac3cd61d600b3314210e9b9893833559
Humi145 <gaels9555@gmail.com>
2026-09-01 12:02:52 +0200

    feat: adding new location

--- app/components/Map/Map.js
+++ app/components/Map/Map.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 function Map() {
     return (
-        <MapContainer center={[200, 300]} zoom={13} style={{ height: '100vh', width: '100%' }} scrollWheelZoom={true}>
+        <MapContainer center={[200, 200]} zoom={13} style={{ height: '100vh', width: '100%' }} scrollWheelZoom={true}>
             <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
