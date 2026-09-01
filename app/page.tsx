8c196bb73f26efb3b77a518f4a51c461724a1634
SkyReox <alexis.roufast-chalada@epitech.eu>
2026-09-01 10:13:07 +0200

    feat: Sidebar
    to use later if needed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Sidebar from "./components/Sidebar";
-
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans bg-main-2">
-      <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white bg-main-2 sm:items-start">
-        <h1 className="text-black text-[50px]">
-          Bienvenue sur <span className="font-semibold">GéoEmploi</span>!
-        </h1>
-      </main>
-    </div>
-  );
+    return (
+        <div className="flex flex-col flex-1 bg-zinc-50 font-sans bg-main-2">
+            <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white bg-main-2 sm:items-start">
+                <h1 className="text-black text-[50px]">
+                    Bienvenue sur <span className="font-semibold">GéoEmploi</span>!
+                </h1>
+            </main>
+        </div>
+    );
 }
