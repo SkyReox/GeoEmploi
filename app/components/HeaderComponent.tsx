c5d533d87823ac8d1577eff53e25f63deb5a29c2
Nathan Amaraggi <nathan.amaraggi@epitech.eu>
2026-09-01 11:29:26 +0200

    feat: adding a button for the map

--- app/components/HeaderComponent.tsx
+++ app/components/HeaderComponent.tsx
@@ -19,6 +19,9 @@ export default function HeaderComponent() {
             <div className="ps-lg gap-xs items-center flex justify-end">
                 <a className="bg-white text-black p-1 rounded-[.5rem] hover:bg-gray-300 shadow-md" href="test">S'inscrire</a> {/* TODO: Change href when Sign in is done */}
             </div>
+            <div className="ps-lg gap-xl items-center flex justify-end">
+                <a className="bg-white text-black p-1 rounded-[.5rem] hover:bg-gray-300 shadow-md" href="map">Carte Interactive</a> {/* TODO: Change href when Sign in is done */}
+            </div>
         </nav>
     </header>);
 }
