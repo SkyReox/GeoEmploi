f6545adc7e44842f5e477f7490296391a2656e0a
Nathan Amaraggi <nathan.amaraggi@epitech.eu>
2026-09-01 11:47:48 +0200

    feat: changing coordinates of the map

--- app/components/Map/Map.js
+++ app/components/Map/Map.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 function Map() {
     return (
-        <MapContainer center={[52.505, -0.09]} zoom={13} style={{ height: '100vh', width: '100%' }} scrollWheelZoom={true}>
+        <MapContainer center={[0, 0]} zoom={13} style={{ height: '100vh', width: '100%' }} scrollWheelZoom={true}>
             <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
