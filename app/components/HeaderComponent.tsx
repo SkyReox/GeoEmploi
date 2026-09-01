8d60307dd1139253595bffc470088535054fa793
SkyReox <alexis.roufast-chalada@epitech.eu>
2026-09-01 11:33:45 +0200

    feat: company icon (top left) is now a button to home page

--- app/components/HeaderComponent.tsx
+++ app/components/HeaderComponent.tsx
@@ -4,14 +4,16 @@ export default function HeaderComponent() {
     return (<header className="flex flex-col p-2 w-full bg-main-1">
         <nav className="flex justify-center">
             <div className="pe-lg flex gap-xs items-center justify-start">
-                <Image
-                    className="w-8"
-                    src="/icon.jpg"
-                    alt="GeoEmploi Icon"
-                    width={100}
-                    height={100}
-                    priority
-                />
+                <a href="/">
+                    <Image
+                        className="w-8"
+                        src="/icon.jpg"
+                        alt="GeoEmploi Icon"
+                        width={100}
+                        height={100}
+                        priority
+                    />
+                </a>
             </div>
             <div className="flex-1 pr-2 items-center flex justify-end">
                 <a className="bg-white text-black p-1 rounded-[.5rem] hover:bg-gray-300 shadow-md" href="map">Carte Interactive</a> {/* TODO: Change href when Sign in is done */}
