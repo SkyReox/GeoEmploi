b595882814022f2af1e886af40304aae6e325cfd
SkyReox <alexis.roufast-chalada@epitech.eu>
2026-09-02 16:50:31 +0200

    feat: more responsive pages

--- app/page.tsx
+++ app/page.tsx
@@ -25,10 +25,10 @@ export default function Home() {
                 <div className="w-full bg-[#f3f2f0] pbs-8 pbe-8 my-8 flex items-center justify-between gap-[1rem]">
                     <div className="flex w-[50%] min-w-[320px] items-center justify-center">
                         <div className="w-[75%]">
-                            <h2 className="text-[#a01e02] text-[30px] font-semibold">
+                            <h2 className="text-[#a01e02] text-[26px] font-semibold">
                                 Attrapez des offres d'emploi idéales!
                             </h2>
-                            <p className="text-black text-[22px] text-justify">Avec la fonctionnalité de <span className="italic">Carte Interactive</span>, vous pouvez retrouver facilement des offres d'emploi autour de vous ou dans une commune précise!</p>
+                            <p className="text-black text-[18px] text-justify">Avec la fonctionnalité de <span className="italic">Carte Interactive</span>, vous pouvez retrouver facilement des offres d'emploi autour de vous ou dans une commune précise!</p>
                         </div>
                     </div>
                     <div className="flex w-[50%] justify-center">
