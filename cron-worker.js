c80c8485a517e9354cb9c1c2e0420a3962b18f09
yanis <yanis.glize@epitech.eu>
2026-09-07 11:57:01 +0200

    fix: remove useless lines

--- cron-worker.js
+++ cron-worker.js
@@ -1,14 +1,11 @@
-// cron-worker.js
 const cron = require('node-cron');
 
 console.log("Planificateur local démarré...");
 
-// S'exécute TOUTES LES MINUTES (Modifiez l'expression cron selon vos besoins)
 cron.schedule('* * 1 * *', async () => {
   console.log(`[${new Date().toLocaleTimeString()}] Déclenchement du cron...`);
 
   try {
-    // Remplacez par votre route exacte et ajoutez un token si sécurisé
     const response = await fetch('http://localhost:3000/api/cron'); 
     const data = await response.json();
     console.log('Réponse du serveur Next.js:', data);
